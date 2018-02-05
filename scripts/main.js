f71d0de34c9e8a65795a25954392acdf97fc8761
KarthikSRam <karthiksram100@gmail.com>
2018-02-04 20:38:00 -0800

    Added images

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,80 @@
-console.log("Sanity check");
+window.onload = function() {
+
+  console.log("Sanity check");
+
+  // Items in our shopping cart
+  var shoppingCart = [];
+
+  // Cereal items
+  var cerealItems = [
+  	{
+  		img: './imgs/cereal/cheerios.jpg',
+  		name: 'Cheerios'
+  	},
+  	{
+  		img: './imgs/cereal/crunch.jpg',
+  		name: 'Crunch'
+  	},
+  	{
+  		img: './imgs/cereal/fruit-loops.jpg',
+  		name: 'Fruit Loops'
+  	},
+  	{
+  		img: './imgs/cereal/puffs.jpg',
+  		name: 'Puffs'
+  	},
+  	{
+  		img: './imgs/cereal/raisin-bran.jpg',
+  		name: 'Raisin Bran'
+  	}
+  ];
+
+  // Meat items
+  var meatItems = [
+  	{
+  		img: './imgs/meats/pepperoni.jpeg',
+  		name: 'Pepperoni'
+  	},
+  	{
+  		img: './imgs/meats/chicken.jpg',
+  		name: 'Chicken'
+  	},
+  	{
+  		img: './imgs/meats/salmon.jpeg',
+  		name: 'Salmon'
+  	},
+  	{
+  		img: './imgs/meats/steak.jpeg',
+  		name: 'Steak'
+  	},
+  	{
+  		img: './imgs/meats/salami.jpg',
+  		name: 'Salami'
+  	}
+  ];
+
+  // Candy items
+  var candyItems = [
+  	{
+  		img: './imgs/candy/kit-kat.jpg',
+  		name: 'Kit Kat'
+  	},
+  	{
+  		img: './imgs/candy/m&ms.jpg',
+  		name: 'M&M\'s'
+  	},
+  	{
+  		img: './imgs/candy/skittles.jpg',
+  		name: 'Skittles'
+  	},
+  	{
+  		img: './imgs/candy/snickers.jpg',
+  		name: 'Snickers'
+  	},
+  	{
+  		img: './imgs/candy/twix.jpg',
+  		name: 'Twix'
+  	}
+  ];
+
+}
